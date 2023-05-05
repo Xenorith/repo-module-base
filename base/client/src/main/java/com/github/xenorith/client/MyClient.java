ad77bd22a4a87f0f7269fc2133a6efb6ffa567a8
Rico Chiu <ricochiu@gmail.com>
2023-05-05 16:54:56 -0700

    Formatting java files

--- base/client/src/main/java/com/github/xenorith/client/MyClient.java
+++ base/client/src/main/java/com/github/xenorith/client/MyClient.java
@@ -8,19 +8,19 @@ import io.grpc.ManagedChannelBuilder;
 
 public final class MyClient {
     public static void main(String[] args) {
-    ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8000)
-        .usePlaintext()
-        .build();
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8000)
+            .usePlaintext()
+            .build();
 
-    HelloServiceGrpc.HelloServiceBlockingStub stub = HelloServiceGrpc.newBlockingStub(channel);
+        HelloServiceGrpc.HelloServiceBlockingStub stub = HelloServiceGrpc.newBlockingStub(channel);
 
-    HelloResponse helloResponse = stub.hello(HelloRequest.newBuilder()
-        .setFirstName("foo")
-        .setLastName("bar")
-        .build());
+        HelloResponse helloResponse = stub.hello(HelloRequest.newBuilder()
+            .setFirstName("foo")
+            .setLastName("bar")
+            .build());
 
-    System.out.println(helloResponse);
+        System.out.println(helloResponse);
 
-    channel.shutdown();
+        channel.shutdown();
     }
 }
